eee6ec2c32006e8dc2afa372f46b120411870290
Gan Yi.ang <getbox@126.com>
2015-06-19 23:37:30 +0800

    creating the messages & worker

---
+++ src/main/java/akka/tutorial/first/java/App.java
@@ -0,0 +1,98 @@
+package akka.tutorial.first.java;
+
+import akka.actor.UntypedActor;
+import akka.util.Duration;
+
+
+/**
+ * Hello Akka!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello Akka!");
+    }
+
+
+    static class Calulate {
+
+    }
+
+    static class Work {
+        private final int start;
+        private final int nrOfElements;
+
+        Work(int start, int nrOfElements) {
+            this.start = start;
+            this.nrOfElements = nrOfElements;
+        }
+
+        public int getStart() {
+            return start;
+        }
+
+        public int getNrOfElements() {
+            return nrOfElements;
+        }
+    }
+
+    static class Result {
+        private final double value;
+
+        Result(double value) {
+            this.value = value;
+        }
+
+        public double getValue() {
+            return value;
+        }
+    }
+
+    static class PiApproximation {
+        private final double pi;
+        private final Duration duration;
+
+
+        PiApproximation(double pi, Duration duration) {
+            this.pi = pi;
+            this.duration = duration;
+        }
+
+        public double getPi() {
+            return pi;
+        }
+
+        public Duration getDuration() {
+            return duration;
+        }
+    }
+
+    public static class Worker extends UntypedActor {
+
+        @Override
+        public void onReceive(Object message) throws Exception {
+            if (message instanceof Work) {
+                Work work = (Work) message;
+                double result = calculatePiFor(work.getStart(), work.getNrOfElements());
+                getSender().tell(new Result(result), getSelf());
+            } else {
+                unhandled(message);
+            }
+
+
+        }
+        private double calculatePiFor(int start, int nrOfElements) {
+            double acc = 0.0;
+            for (int i = start * nrOfElements; i <= ((start + 1) * nrOfElements - 1); i++) {
+                acc += 4.0 * (1 - (i % 2) * 2) / (2 * i + 1);
+            }
+            return 0;
+        }
+
+
+    }
+
+
+}
+
+
+
